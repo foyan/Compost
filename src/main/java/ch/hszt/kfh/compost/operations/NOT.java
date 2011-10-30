56b26c6abff68e3c588385bb0c105eb0b77fbc0f
Florian <vlo@guez.li>
2011-10-30 15:30:06 +0100

    Boolean operations

--- src/main/java/ch/hszt/kfh/compost/operations/NOT.java
+++ src/main/java/ch/hszt/kfh/compost/operations/NOT.java
@@ -6,35 +6,31 @@ import ch.hszt.kfh.compost.Compost;
 import ch.hszt.kfh.compost.MemCell;
 import ch.hszt.kfh.compost.RegisterId;
 
-public class AND extends Operation {
+public class NOT extends Operation {
 
 	@Override
 	protected String opCodeString() {
-		return "0000xx100xxxxxxx";
+		return "00000000xxxxxxxx";
 	}
 
 	@Override
 	public String mnemonic() {
-		return "AND";
+		return "NOT";
 	}
 
 	@Override
 	public void exec(Compost compost, boolean[] argument) throws Exception {
 		MemCell reg1 = compost.getRegister(RegisterId.ACCUM);
-		MemCell reg2 = getRegister(compost, new boolean[] { argument[0], argument[1] });
 		for (int i = 0; i < reg1.getSize(); i++) {
-			reg1.setBit(i, reg1.getBit(i) && reg2.getBit(i));
+			reg1.setBit(i, !reg1.getBit(i));
 		}
 	}
 
 	@Override
 	public boolean[] addArguments(boolean[] opCode, List<String> arguments) throws Exception {
-		if (arguments.size() != 1) {
-			throw new Exception("AND requires one argument.");
+		if (arguments.size() != 0) {
+			throw new Exception("NOT does not require any arguments.");
 		}
-		boolean[] id = getRegisterIdArgumentFromMnemonic(arguments.get(0));
-		opCode[4] = id[0];
-		opCode[5] = id[1];
 		return opCode;
 	}
 
--- src/main/java/ch/hszt/kfh/compost/operations/OR.java
+++ src/main/java/ch/hszt/kfh/compost/operations/OR.java
@@ -6,16 +6,16 @@ import ch.hszt.kfh.compost.Compost;
 import ch.hszt.kfh.compost.MemCell;
 import ch.hszt.kfh.compost.RegisterId;
 
-public class AND extends Operation {
+public class OR extends Operation {
 
 	@Override
 	protected String opCodeString() {
-		return "0000xx100xxxxxxx";
+		return "0000xx110xxxxxxx";
 	}
 
 	@Override
 	public String mnemonic() {
-		return "AND";
+		return "OR";
 	}
 
 	@Override
@@ -23,14 +23,14 @@ public class AND extends Operation {
 		MemCell reg1 = compost.getRegister(RegisterId.ACCUM);
 		MemCell reg2 = getRegister(compost, new boolean[] { argument[0], argument[1] });
 		for (int i = 0; i < reg1.getSize(); i++) {
-			reg1.setBit(i, reg1.getBit(i) && reg2.getBit(i));
+			reg1.setBit(i, reg1.getBit(i) || reg2.getBit(i));
 		}
 	}
 
 	@Override
 	public boolean[] addArguments(boolean[] opCode, List<String> arguments) throws Exception {
 		if (arguments.size() != 1) {
-			throw new Exception("AND requires one argument.");
+			throw new Exception("OR requires one argument.");
 		}
 		boolean[] id = getRegisterIdArgumentFromMnemonic(arguments.get(0));
 		opCode[4] = id[0];
